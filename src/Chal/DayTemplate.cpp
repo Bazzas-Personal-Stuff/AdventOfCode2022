1dfe475108b16f8717400084c001488bc9ee790a
Bazzagibbs <bailey.gibbons2018@gmail.com>
2022-12-01 18:21:49 +1100

    Added Day template

---
+++ src/Chal/DayTemplate.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "DayTemplate.h"
+#include "gtest/gtest.h"
+
+std::string DayTemplate::Stage1(std::string& input) {
+    using namespace std;
+    stringstream ss(input);
+    stringstream outSS;
+    
+    
+    return outSS.str();
+}
+
+std::string DayTemplate::Stage2(std::string& input) {
+    using namespace std;
+    stringstream ss(input);
+    stringstream outSS;
+
+    
+    return outSS.str();
+}
+
+// TEST(DayTemplateTest, Stage1) {
+//     DayTemplate day = DayTemplate();
+//     
+//     std::string input(R"()");
+//     std::string expected(R"()");
+//
+//     ASSERT_EQ(expected, day.Stage1(input));
+// }
+//
+// TEST(DayTemplateTest, Stage2) {
+//     DayTemplate day = DayTemplate();
+//     
+//     std::string input(R"()");
+//     std::string expected(R"()");
+//
+//     ASSERT_EQ(expected, day.Stage1(input));
+// }
+
+
---
+++ src/Chal/DayTemplate.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "Chal.h"
+#include <string>
+
+class DayTemplate : public Chal{
+public:
+    virtual std::string Stage1(std::string& input) override;
+    virtual std::string Stage2(std::string& input) override;
+};
