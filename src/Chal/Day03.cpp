41d5a30412265ccd343ed4fae75699cacf798e3e
Bazzagibbs <bailey.gibbons2018@gmail.com>
2022-12-02 21:26:11 +1100

    Added Rider file template for AdventOfCode days

---
+++ src/Chal/Day03.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "Day03.h"
+#include "gtest/gtest.h"
+
+std::string Day03::Stage1(std::istream &input) {
+    using namespace std;
+    stringstream outSS;
+
+    outSS << "Stage1 not implemented";
+
+    return outSS.str();
+}
+
+std::string Day03::Stage2(std::istream &input) {
+    using namespace std;
+    stringstream outSS;
+
+    outSS << "Stage2 not implemented";
+
+    return outSS.str();
+}
+
+
+// =============== gtests for given cases ===============
+
+TEST(Day03, Stage1) {
+    Day03 day = Day03();
+    std::string input(R"()");
+
+    std::stringstream inputSS(input);
+    std::string expected("Test not implemented");
+
+    ASSERT_EQ(expected, day.Stage1(inputSS));
+}
+
+TEST(Day03, Stage2) {
+    Day03 day = Day03();
+    std::string input(R"()");
+
+    std::stringstream inputSS(input);
+    std::string expected("Test not implemented");
+
+    ASSERT_EQ(expected, day.Stage2(inputSS));
+}
---
+++ src/Chal/Day03.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "Chal.h"
+#include <string>
+
+class Day03 : public Chal {
+public:
+    virtual std::string Stage1(std::istream &input) override;
+    virtual std::string Stage2(std::istream &input) override;
+    
+};
