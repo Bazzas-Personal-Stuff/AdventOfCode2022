75eba4cca63af027abd16748e5370ad751867b56
Bazzagibbs <bailey.gibbons2018@gmail.com>
2022-12-06 01:00:05 +1100

    Launcher no longer exits after running a challenge

--- src/Launcher.cpp
+++ src/Launcher.cpp
@@ -6,7 +6,7 @@
 #include "Chal/Day04.h"
 #include "Chal/Day05.h"
 
-std::filesystem::path inputsDir = "inputs";
+const std::filesystem::path inputsDir = "inputs";
 
 std::vector<std::unique_ptr<Chal>> days;
 void PopulateDays() {
@@ -24,52 +24,57 @@ int main(int argc, char** argv) {
     
     int dayInput;
     int stageInput;
+
     while(true) {
-        std::cout << "Day [1-"<< days.size() <<"]: ";
-        std::cin >> dayInput;
+        while(true) {
+            std::cout << "===========================" << std::endl;
+            std::cout << "Day [1-"<< days.size() <<"]: ";
+            std::cin >> dayInput;
 
 
-        // if not in range of available day solutions
-        if(!(dayInput > 0 && dayInput <= (int)days.size())) {
-            continue;
+            // if not in range of available day solutions
+            if(!(dayInput > 0 && dayInput <= (int)days.size())) {
+                continue;
+            }
+
+            std::cout << "Stage [1 or 2, 0 to test]: ";
+            std::cin >> stageInput;
+
+            if(stageInput == 0) {
+                testing::InitGoogleTest(&argc, argv);
+                ::testing::GTEST_FLAG(filter) = std::format("Day{:02d}*", dayInput);
+                RUN_ALL_TESTS();
+                continue;
+            }
+            // if stage isn't 1 or 2
+            if(!(stageInput == 1 || stageInput == 2)) {
+                continue;
+            }
+
+            break;
         }
 
-        std::cout << "Stage [1 or 2, 0 to test]: ";
-        std::cin >> stageInput;
+        std::cout << "\n===== Day " << dayInput << ", Stage " << stageInput <<" =====" << std::endl;
 
-        if(stageInput == 0) {
-            testing::InitGoogleTest(&argc, argv);
-            ::testing::GTEST_FLAG(filter) = std::format("Day{:02d}*", dayInput);
-            return RUN_ALL_TESTS();
+        // input file to string
+        std::string fileName = std::format("input{:02d}.txt", dayInput);
+        std::filesystem::path p = std::filesystem::current_path();
+        p /= inputsDir / fileName;
+
+        std::ifstream input(p);
+        // std::stringstream buffer;
+        // buffer << t.rdbuf();
+        // std::string input = buffer.str();
+
+        // Run the requested stage
+        dayInput -= 1; // 1-index to 0-index
+        if(stageInput == 1) {
+            std::cout << days[dayInput]->Stage1(input) << std::endl;
         }
-        // if stage isn't 1 or 2
-        if(!(stageInput == 1 || stageInput == 2)) {
-            continue;
+        else {
+            std::cout << days[dayInput]->Stage2(input) << std::endl;
         }
+        input.close();
 
-        break;
     }
-
-    std::cout << "\n===== Day " << dayInput << ", Stage " << stageInput <<" =====" << std::endl;
-
-    // input file to string
-    std::string fileName = std::format("input{:02d}.txt", dayInput);
-    inputsDir /= fileName;
-    std::filesystem::path p = std::filesystem::current_path();
-    p /= inputsDir;
-
-    std::ifstream input(p);
-    // std::stringstream buffer;
-    // buffer << t.rdbuf();
-    // std::string input = buffer.str();
-
-    // Run the requested stage
-    dayInput -= 1; // 1-index to 0-index
-    if(stageInput == 1) {
-        std::cout << days[dayInput]->Stage1(input) << std::endl;
-    }
-    else {
-        std::cout << days[dayInput]->Stage2(input) << std::endl;
-    }
-
 }
