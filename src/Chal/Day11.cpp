0833eaee97d09122007713d90908f98ae26212d1
Bazzagibbs <bailey.gibbons2018@gmail.com>
2022-12-11 18:56:19 +1100

    Day 11 part 1

--- src/Chal/Day11.cpp
+++ src/Chal/Day11.cpp
@@ -2,21 +2,146 @@
 #include "Day11.h"
 #include "gtest/gtest.h"
 
+Day11::Operation::Operation(const std::string& lhs, const std::string& rhs, const char op) {
+    if(lhs == "old") {
+        m_LhsOperand = std::make_unique<IdentifierOperand>();
+    }
+    else {
+        m_LhsOperand = std::make_unique<ConstOperand>(std::stoi(lhs));
+    }
+    
+    if(rhs == "old") {
+        m_RhsOperand = std::make_unique<IdentifierOperand>();
+    }
+    else {
+        m_RhsOperand = std::make_unique<ConstOperand>(std::stoi(rhs));
+    }
+
+    switch (op) {
+    case '+':
+        m_Op = Add;
+        break;
+    case '-':
+        m_Op = Sub;
+        break;
+    case '*':
+        m_Op = Mul;
+        break;
+    case '/':
+        m_Op = Div;
+        break;
+    default:
+        m_Op = Add;
+        break;
+    }
+}
+
+long Day11::Operation::Eval(long oldVal) const {
+    long lhs = m_LhsOperand->Eval(oldVal);
+    long rhs = m_RhsOperand->Eval(oldVal);
+
+    switch(m_Op) {
+    case Add: return lhs + rhs;
+    case Sub: return lhs - rhs;
+    case Mul: return lhs * rhs;
+    case Div: return lhs / rhs;
+    default: return -9999999;
+    }
+}
+
+std::vector<Day11::Monkey> Day11::ParseMonkeys(std::istream& input) {
+    using namespace std;
+    vector<Monkey> monkeys;
+
+    string garbage;
+    while(input >> garbage >> garbage >> garbage >> garbage) {
+        Monkey& monkey = monkeys.emplace_back();
+
+        // Starting items
+        string startingItemsStr;
+        getline(input, startingItemsStr);
+        stringstream startingItemsSS(startingItemsStr);
+        string item;
+        while(getline(startingItemsSS, item, ',')) {
+            monkey.items.push_back(stoi(item));
+        }
+        
+        // Operation
+        char op;
+        string lhs, rhs;
+        input >> garbage >> garbage >> garbage >> lhs >> op >> rhs;
+        monkey.operation = make_unique<Operation>(lhs, rhs, op);
+
+        // Get test value
+        input >> garbage >> garbage >> garbage >> monkey.testVal;
+
+        // Get targets
+        input >> garbage >> garbage >> garbage >> garbage >> garbage >> monkey.trueTarget;
+        input >> garbage >> garbage >> garbage >> garbage >> garbage >> monkey.falseTarget;
+    }
+
+    return monkeys;
+}
+
+void Day11::EvalMonkeys(std::vector<Day11::Monkey>& monkeys, int iterCount, int divide, long testProduct) {
+    for(int round = 0; round < iterCount; round++) {
+        for(int monkeyIdx = 0; monkeyIdx < monkeys.size(); monkeyIdx++) {
+            Monkey& monkey = monkeys[monkeyIdx];
+            
+            // iterate items backwards so we can pop
+            for(int i = monkey.items.size() - 1; i >= 0; i--) {
+                monkey.inspectCount++;
+                // Inspect item
+                long worryLevel = monkey.items[i];
+                monkey.items[i] = (monkey.operation->Eval(worryLevel) / divide) % testProduct;
+                // if(monkey.items[i] > testProduct || monkey.items[i] < 0) {
+                //     int item = monkey.items[i];
+                //     std::cout << item << std::endl;
+                // }
+                // Test
+                if(monkey.items[i] % monkey.testVal == 0) {
+                    monkeys[monkey.trueTarget].items.push_back(monkey.items[i]);
+                }
+                else {
+                    monkeys[monkey.falseTarget].items.push_back(monkey.items[i]);
+                }
+                monkey.items.pop_back();
+            }
+        }
+    }
+}
+
 std::string Day11::Stage1(std::istream& input) {
     using namespace std;
     stringstream outSS;
+    
+    vector<Monkey> monkeys = ParseMonkeys(input);
+    long testProduct = 1;
+    for(Monkey& monkey : monkeys) {
+        testProduct *= monkey.testVal;
+    }
+    EvalMonkeys(monkeys, 20, 3, testProduct);
 
-    outSS << "Stage1 not implemented";
-
+    // Output answer
+    sort(monkeys.begin(), monkeys.end(), [](Monkey& a, Monkey& b){ return a.inspectCount > b.inspectCount;});
+    outSS << monkeys[0].inspectCount * monkeys[1].inspectCount;
     return outSS.str();
 }
 
 std::string Day11::Stage2(std::istream& input) {
     using namespace std;
     stringstream outSS;
+    
+    vector<Monkey> monkeys = ParseMonkeys(input);
+    long testProduct = 1;
+    for(Monkey& monkey : monkeys) {
+        testProduct *= monkey.testVal;
+    }
+    EvalMonkeys(monkeys, 10000, 1, testProduct);
 
-    outSS << "Stage2 not implemented";
-
+    // Output answer
+    sort(monkeys.begin(), monkeys.end(), [](Monkey& a, Monkey& b){ return a.inspectCount > b.inspectCount;});
+    outSS << monkeys[0].inspectCount * monkeys[1].inspectCount;
     return outSS.str();
 }
 
@@ -27,7 +152,7 @@ TEST(Day11, Stage1) {
     Day11 day = Day11();
 
     std::stringstream input = day.GetGivenInput();
-    std::string expected("Test not implemented");
+    std::string expected("10605");
 
     ASSERT_EQ(expected, day.Stage1(input));
 }
@@ -36,7 +161,7 @@ TEST(Day11, Stage2) {
     Day11 day = Day11();
 
     std::stringstream input = day.GetGivenInput();
-    std::string expected("Test not implemented");
+    std::string expected("2713310158");
 
     ASSERT_EQ(expected, day.Stage2(input));
 }
--- src/Chal/Day11.h
+++ src/Chal/Day11.h
@@ -4,11 +4,91 @@
 
 class Day11 : public Chal {
 public:
+    enum Operator {
+        Add,
+        Sub,
+        Mul,
+        Div,
+    };
+    
+    class Operand {
+    public:
+        virtual ~Operand() = default;
+        virtual long Eval(const long oldVal) const = 0;
+    };
+
+    class ConstOperand : public Operand {
+    public:
+        ConstOperand(long val) : m_Val(val) {}
+        virtual long Eval(const long oldVal) const override { return m_Val; }
+    private:
+        long m_Val;
+    };
+
+    class IdentifierOperand : public Operand {
+    public:
+        IdentifierOperand() = default;
+        virtual long Eval(const long oldVal) const override { return oldVal; }
+    };
+    
+    class Operation {
+    public:
+        Operation(const std::string& lhs, const std::string& rhs, const char op);
+        long Eval(long oldVal) const;
+        
+    private:
+        std::unique_ptr<Operand> m_LhsOperand;
+        std::unique_ptr<Operand> m_RhsOperand;
+        Operator m_Op;
+    };
+    
+    class Monkey {
+    public:
+        Monkey() = default;
+        
+        std::vector<long> items = std::vector<long>();
+
+        std::unique_ptr<Operation> operation = nullptr;
+        long testVal = 0;
+        int trueTarget = 0;
+        int falseTarget = 0;
+
+        long inspectCount = 0;
+    };
+
+    static std::vector<Day11::Monkey> ParseMonkeys(std::istream& input);
+    static void EvalMonkeys(std::vector<Monkey>& monkeys, int iterCount, int divide, long testProduct);
     virtual std::string Stage1(std::istream& input) override;
     virtual std::string Stage2(std::istream& input) override;
 
     virtual std::stringstream GetGivenInput() override {
-        return std::stringstream(R"()");
+        return std::stringstream(R"(Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1)");
     }
 
     
